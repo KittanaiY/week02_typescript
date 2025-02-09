c927f53a41f7fff99f705077b37c79fa95904d61
kittanai <kittanai_yam@cmu.ac.th>
2025-02-09 15:17:14 +0700

    Add task05-07 file

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/test', (req: Request, res:Response) => {
+    const id = req.query.id;
+    const output = `id: ${id}`;
+    res.send(output);
+})
+
+app.listen(port, () => {
+    console.log('App listening at http://localhost:${port}')
+})
