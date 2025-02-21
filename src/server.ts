c538d39ba30af5350f51ab7ea2db788ead279ee1
kittanai <kittanai_yam@cmu.ac.th>
2025-02-21 16:42:00 +0700

    Add Task13: Refactor Route

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import express, {Request, Response} from 'express';
-import { getAllEvents, getEventByCategory, getEventById, addEvent } from "./services/EventService";
-import type { Event } from './models/Event';
 import multer from 'multer';
 import dotenv from 'dotenv';
+import eventRoute from './routes/EventRoute';
 dotenv.config();
 import { uploadFile } from './services/UploadFileService';
 
 const app = express(); // recieve port 3000
-const port = 3000;
-
 app.use(express.json());
-
+app.use('/events', eventRoute);
+const port = 3000;
 
 app.get('/test', (req: Request, res:Response) => {
     const id = req.query.id;
@@ -21,32 +19,6 @@ app.get('/test', (req: Request, res:Response) => {
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
-
-app.get("/events", async(req,res) => {
-    if (req.query.category) {
-        const category = req.query.category;
-        const filteredEvents =await getEventByCategory(category as string);
-        res.json(filteredEvents);
-    } else {
-        res.json(await getAllEvents());
-    }
-});
-
-app.get("/events/:id", async(req,res) => {
-    const id = parseInt(req.params.id); //conv id from req into int
-    const event =await getEventById(id);
-    if (event) {
-        res.json(event);
-    } else{
-        res.status(404).send("Event not found");
-    }
-})
-
-app.post("/events", async(req,res) => {
-    const newEvent: Event = req.body;
-    await addEvent(newEvent);
-    res.json(newEvent);
-});
 
 const upload = multer({ storage: multer.memoryStorage() });
 
